d7421f3958de8c3ef83b802d1796cca69ce99c78
tcle103 <146970839+tcle103@users.noreply.github.com>
2024-06-12 12:52:20 -0700

    Added + set up initial graphical elements
    Added the primary graphical assets in the main gameplay loop and set up sprites for them

--- src/Scenes/main.js
+++ src/Scenes/main.js
@@ -10,15 +10,49 @@ class Main extends Phaser.Scene {
     preload() {
         // Assets from Kenny Assets pack "Shape Characters"
         this.load.setPath("./assets/");
+
+        this.load.image("sky", "sky.png");
+        this.load.image("bg1", "bg1.png");
+        this.load.image("bg2", "bg2.png");
+        this.load.image("ground","ground.png");
+        this.load.image("foreground", "foreground.png");
+
+        this.load.image("car", "rounded_red_edited.png");
         
     }
 
     create() {
         let my = this.my;   // create an alias to this.my for readability
+
+        my.sprite.sky = this.add.sprite(3780, 310, "sky");
+        my.sprite.sky.setScale(10);
+
+        my.sprite.bg1 = this.add.sprite(3780, 310, "bg1");
+        my.sprite.bg1.setScale(10);
+
+        my.sprite.bg2 = this.add.sprite(3780, 310, "bg2");
+        my.sprite.bg2.setScale(10);
+
+        my.sprite.ground = this.add.sprite(3780, 310, "ground");
+        my.sprite.ground.setScale(10);
+
+        my.sprite.fground = this.add.sprite(3780, 310, "foreground");
+        my.sprite.fground.setScale(10);
+
+        my.sprite.car = this.add.sprite(400, 360, "car");
+        my.sprite.car.setScale(10);
+
+        this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        this.rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
     }
 
     update() {
         let my = this.my;    // create an alias to this.my for readability
+
+        if ((this.dKey.isDown || this.rightKey.isDown)) {
+            
+        }
 
     }
 
